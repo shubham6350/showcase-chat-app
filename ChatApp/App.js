19ac31643047a15f24a6e5da929a52090bb92a0a
yashraj-anand-B687 <yash3981@gmail.com>
2022-10-26 18:55:42 +0530

    Added theming

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { useColorScheme } from 'react-native';
+import {
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
+import { COLORS } from './assets/colors/colors';
+
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: COLORS.MAIN_PRIMARY,
+  },
+};
 
 export class App extends Component {
+  
   render() {
     return (
       <>
