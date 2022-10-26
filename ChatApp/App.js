d2c9316b94f61545f2517cf829db55847395153f
yashraj-anand-B687 <yash3981@gmail.com>
2022-10-26 19:22:08 +0530

    Fonts added

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { useColorScheme } from 'react-native';
-import {
-  DefaultTheme,
-  DarkTheme,
-} from '@react-navigation/native';
+// import {
+//   DefaultTheme,
+//   DarkTheme,
+// } from '@react-navigation/native';
 import { COLORS } from './assets/colors/colors';
 
 
-const MyTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: COLORS.MAIN_PRIMARY,
-  },
-};
+// const MyTheme = {
+//   ...DefaultTheme,
+//   colors: {
+//     ...DefaultTheme.colors,
+//     primary: COLORS.MAIN_PRIMARY,
+//   },
+// };
 
 export class App extends Component {
   
@@ -22,7 +22,9 @@ export class App extends Component {
     return (
       <>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Welcome To ChatApp!</Text>
+          <Text style={{fontFamily: 'Urbanist-Regular'}}>Welcome To ChatApp!</Text>
+          <Text style={{fontFamily: 'Urbanist-Medium'}}>Welcome To ChatApp!</Text>
+          <Text style={{fontFamily: 'Urbanist-Bold'}}>Welcome To ChatApp!</Text>
         </View>
       </>
     );
