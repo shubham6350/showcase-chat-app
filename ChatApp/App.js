7f6fcff5b3ced2a6805595ccc8991e462bee8e6f
yashraj-anand-B687 <yash3981@gmail.com>
2022-10-28 12:35:53 +0530

    test

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Text, TextInput, View } from 'react-native';
+import WelcomeUI from './src/screens/WelcomeUI';
 
 import SplashScreen from 'react-native-splash-screen';
 
@@ -10,10 +11,11 @@ const App = () => {
 
   return (
     <>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {/* <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> */}
         {/* <Splash_Screen /> */}
-        <Text>Welcome to ChatApp!</Text>
-      </View>
+        {/* <Text>Welcome to ChatApp!</Text> */}
+      {/* </View> */}
+      <WelcomeUI />
     </>
   );
 };
