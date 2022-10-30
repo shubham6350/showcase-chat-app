e70346e9774f9eb24f86c2f7967030b410929424
chitz <54183293+shubham6350@users.noreply.github.com>
2022-10-31 03:03:32 +0530

    dummy

--- ChatApp/src/screens/Welcome_screen.js
+++ ChatApp/src/screens/Welcome_screen.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 const Welcome_screen = () => {
   return (
     <View>
-      <Text> textInComponent </Text>
+      <Text> textInC </Text>
     </View>
   );
 };
